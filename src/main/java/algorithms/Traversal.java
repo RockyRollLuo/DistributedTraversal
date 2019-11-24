e770c8a06db2db4c4256b392d366c2918aaaf213
RockyRoll <luoqi4110217@hotmail.com>
2019-11-24 14:44:54 +0800

    traversal

--- src/main/java/algorithms/Traversal.java
+++ src/main/java/algorithms/Traversal.java
@@ -1,33 +1,56 @@
 package algorithms;
 
 import org.apache.log4j.Logger;
+import util.Edge;
 import util.Graph;
 
 import java.util.Hashtable;
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Traversal {
-
     private static Logger LOGGER = Logger.getLogger(Traversal.class);
 
     private Graph graph;
+    private Hashtable<Integer, Integer> pcd;
+    private Hashtable<Integer, Integer> mcd;
 
+
+
+    //
     public Traversal(Graph graph) {
         this.graph = graph;
     }
 
-    public void computePCD() {
+    private void computePCD() {
         Hashtable<Integer, LinkedList<Integer>> adjMap = graph.getAdjMap();
 
-        for (Integer node:adjMap.keySet()) {
+        for (Integer node : adjMap.keySet()) {
 
         }
+    }
+
+
+    private void computeMCD() {
 
 
     }
 
+    public void runInsert(Graph graph) {
 
+    }
+
+    public void runInsert(Graph graph, Edge e0) {
+
+    }
+
+    public void runDeletion(Graph graph) {
+
+
+
+    }
+
+    public void runDeletion(Graph graph, Edge e0) {
+
+    }
 
 }
